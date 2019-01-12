0e8fc6373d8dd626ad3615eb642282e148e45454
Rajesh <29378061+the-13th@users.noreply.github.com>
2019-01-13 02:29:36 +0530

    nth

---
+++ ex6.py
@@ -0,0 +1,21 @@
+
+x = "There are %d types of people" % 10 #simple use of %d
+binary = "binary" #simple variable assigning
+do_not = "don't" #same
+y = "those who know %s and those who %s." % (binary, do_not) # using multiple formatters
+
+print x
+print y
+
+print "i said: %r." % x # using %r which is basically using the whole statement
+print "i also said: %r." %y
+
+hilarious = False #variable
+joke_evslustion = "Isnt that joke so funny ?!. %r " #using %r for next line
+
+print joke_evslustion % hilarious #very smartly used %r for advantage just by one variable adjustment, for eg, we could write any variable at place of hialrious at the last moment 
+
+w = "this is the left side of..."
+e = "a string with a right side."
+
+print w + e # strings in one string 
